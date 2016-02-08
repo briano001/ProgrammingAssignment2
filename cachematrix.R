917da8e4354b80073d1e61a4602c2fdd9297352d
briano001 <briano001@yahoo.com>
2016-02-07 19:37:26 -0500

    Programming Assignment 2 - Updated
    debugged version (too many curly braces)

--- cachematrix.R
+++ cachematrix.R
@@ -4,83 +4,47 @@
 ## calculated (and the matrix has not changed), then the cachesolve 
 ## should retrieve the inverse from the cache.
 
-makeCacheMatrix <- function(x = matrix()) 
-	{
+makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
     
-
-set <- function(y) {
-
-  x <<- y
-
-  inv <<- NULL
-
- }
-
- 
-# gets the matrix
-
- get <- function() x
-
-
-#sets and then gets the inverse 
- setinverse <- function(inverse) inv <<- inverse
-
- getinverse <- function() inv
-
-
-#captures within a list 
- list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
-
+	set <- function(y) {
+	  x <<- y
+	  inv <<- NULL
+	 }
+	# gets the matrix
+	 get <- function() x
+	
+	
+	#sets and then gets the inverse 
+	 setinverse <- function(inverse) inv <<- inverse
+	
+	 getinverse <- function() inv
+	
+	#captures within a list 
+	 list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
-
-
-
-
-
-
 
 #CacheSolve will do the needed calculations to to compute the inverse and cache the result
+ cacheSolve <- function(x, ...) {
+	#check to see if the inverse is already computed    
+	    inv <- x$getinverse()
+	
+	    if(!is.null(inv)) {
+	
+	        message("getting cached data.")
+	
+	        return(inv)
+	    }
 
-
-
-
-
-
-
-
-
-cacheSolve <- function(x, ...) 
-{
-
-
-#check to see if the inverse is already computed    
-    inv <- x$getinverse()
-
-    if(!is.null(inv)) {
-
-        message("getting cached data.")
-
-        return(inv)
-
-    }
-
-    
     data <- x$get()
     
-
-#Compute the Inverse
-    inv <- solve(data)
-
-
-#Cache the result    
-    x$setinverse(inv)
-    
-# Print out the result    
-
-    inv
-
-}
-
+	#Compute the Inverse
+	    inv <- solve(data)
+	
+	#Cache the result    
+	    x$setinverse(inv)
+	    
+	# Print out the result    	
+	    inv
 }
 
